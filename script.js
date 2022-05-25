2ceae9f2a8c23778121b383d1dde1e60b33a9ff8
DylanNDang <82989695+DylanDangUW@users.noreply.github.com>
2022-05-24 19:54:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ function roll(){
         });
         let dieOneValue = Math.floor(Math.random()*6);
         let dieTwoValue = Math.floor(Math.random()*6);
-        let dieThreeValue = Math.floor(Math.random()*6);
-        let dieFourValue = Math.floor(Math.random()*6);
-        let dieFiveValue = Math.floor(Math.random()*6);
+        //let dieThreeValue = Math.floor(Math.random()*6);
+        //let dieFourValue = Math.floor(Math.random()*6);
+        //let dieFiveValue = Math.floor(Math.random()*6);
         console.log(dieOneValue,dieTwoValue,dieThreeValue,dieFourValue,dieFiveValue);
         document.querySelector("#die-1").setAttribute("src", images[diceOneValue]);
         document.querySelector("#die-2").setAttribute("src", images[diceTwoValue]);
-        document.querySelector("#die-3").setAttribute("src", images[diceThreeValue]);
-        document.querySelector("#die-4").setAttribute("src", images[diceFourValue]);
-        document.querySelector("#die-5").setAttribute("src", images[diceFiveValue]);
+        //document.querySelector("#die-3").setAttribute("src", images[diceThreeValue]);
+       // document.querySelector("#die-4").setAttribute("src", images[diceFourValue]);
+        //document.querySelector("#die-5").setAttribute("src", images[diceFiveValue]);
     },
     1000
     );
