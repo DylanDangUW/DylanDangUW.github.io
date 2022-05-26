4654a65d0ef6a9fa9e03910e1c8191696ab09f94
DylanNDang <82989695+DylanDangUW@users.noreply.github.com>
2022-05-26 15:56:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let images = ["dice-01.svg",
-"dice-02.svg",
-"dice-03.svg",
-"dice-04.svg",
-"dice-05.svg",
-"dice-06.svg"];
+let images = ["dice-01.svg","dice-02.svg","dice-03.svg","dice-04.svg","dice-05.svg","dice-06.svg"];
 let dice = document.querySelectorAll("img");
 
 function roll(){
