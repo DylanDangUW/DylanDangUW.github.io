98c3fae8706133bfc4edc02af14bc7861159189a
DylanNDang <82989695+DylanDangUW@users.noreply.github.com>
2022-05-26 15:40:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let image = ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/150px-Dice-1-b.svg.png",
-             "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Dice-2-b.svg/150px-Dice-2-b.svg.png",
-             "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/150px-Dice-3-b.svg.png",
-             "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Dice-4-b.svg/150px-Dice-4-b.svg.png",
-             "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Dice-5-b.svg/150px-Dice-5-b.svg.png",
-             "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/768px-Dice-6-b.svg.png"];
+let images = ["dice-01.svg",
+"dice-02.svg",
+"dice-03.svg",
+"dice-04.svg",
+"dice-05.svg",
+"dice-06.svg"];
 let dice = document.querySelectorAll("img");
 
 function roll(){
@@ -19,10 +19,7 @@ function roll(){
         console.log(dieOneValue,dieTwoValue);
         document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
         document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-        //document.querySelector("#die-3").setAttribute("src", images[dieThreeValue]);
-        //document.querySelector("#die-4").setAttribute("src", images[dieFourValue]);
-        //document.querySelector("#die-5").setAttribute("src", images[dieFiveValue]);
-        //document.querySelector("#total").innerHTML = "Your roll is " + ( (dieOneValue +1) + (dieTwoValue + 1) );
+        document.querySelector("#total").innerHTML = "Your roll is " + ( (dieOneValue +1) + (dieTwoValue + 1) );
     },
     1000
     );
